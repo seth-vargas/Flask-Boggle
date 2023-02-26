966e7c5ca2798240d5f5e3269d072347b1003a79
Seth Vargas <shvargas01@gmail.com>
2023-02-26 16:18:54 -0600

    added static and templates folders, and updated .gitignore and app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+from flask import Flask, redirect, render_template, session
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "some secret"
+
+@app.route('/')
+def index():
+    board = Boggle()
+    session['board'] = board.make_board()
+    return render_template('board.html', board=session['board'])
+
+
